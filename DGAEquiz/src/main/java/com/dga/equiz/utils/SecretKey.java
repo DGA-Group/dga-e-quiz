f782248050f35bdf380391b9c698011a9557ccc3
Nguyen Tuan Duong <tuanduong152004@gmail.com>
2023-10-08 16:38:28 +0700

    Remove SecretKey from source

--- DGAEquiz/src/main/java/com/dga/equiz/utils/SecretKey.java
+++
@@ -1,9 +0,0 @@
-package com.dga.equiz.utils;
-
-public class SecretKey {
-    public static final String HOST = "localhost";
-    public static final String PORT = "3307";
-    public static final String DATABASE = "jdbcdemo";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
-}
