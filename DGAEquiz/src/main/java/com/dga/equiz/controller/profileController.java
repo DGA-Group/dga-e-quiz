5fdafdd8c6b1dbbc7d0608ae631878a106fe48dc
Tuan_Anh <tranduytuana123@gmail.com>
2023-10-15 11:29:01 +0700

    The circle avatar in profile.

---
+++ DGAEquiz/src/main/java/com/dga/equiz/controller/profileController.java
@@ -0,0 +1,21 @@
+package com.dga.equiz.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class profileController implements Initializable {
+    @FXML
+    private Circle circle;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image img = new Image(String.valueOf(getClass().getResource("/image/test.jpg")));
+        circle.setFill(new ImagePattern(img));
+    }
+}
