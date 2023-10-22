b91b7e425fd0a9a15239b0ba9eae50343f6fb4b9
Nguyen Tuan Duong <tuanduong152004@gmail.com>
2023-10-22 17:52:22 +0700

    Add color enum

--- DGAEquiz/src/main/java/com/dga/equiz/utils/ApplicationEnum.java
+++ DGAEquiz/src/main/java/com/dga/equiz/utils/ApplicationEnum.java
@@ -1,5 +1,7 @@
 package com.dga.equiz.utils;
 
+import java.util.HashMap;
+
 public class ApplicationEnum {
 
     //region Question type
@@ -11,9 +13,23 @@ public class ApplicationEnum {
     }
     //endregion
 
+    //region Anchor type
     public enum AnchorType {
         FitToParent,
         Center
+    }
+    //endregion
+
+    public static class Color{
+        // Tỉ lệ màu : 60 30 10.
+        public static final String WhiteSmoke = "#F5F8FA";      // Trắng 60%
+        public static final String LightSkyBlue = "#C9E6FF";    // Xanh lam pastel 30%
+        public static final String Aquamarine = "#B4DDD3";      // Xanh lá pastel 30%
+        public static final String Goldenrod = "#FFC75A";       // Vàng 10%
+        public static final String LightGray = "#C4C4C4";       // Xám 1%
+        public static final String DarkBlue = "#5D8DBF";       // Xanh lam tối 1%
+        public static final String MidnightBlue = "#444766";    // Xanh lam đêm 1%
+        public static final String Black = "#000000";           // Đen 1%
     }
 
 
